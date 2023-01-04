2deb829258e8e6d382823caebff5d6fbc494301e
cristobal2215 <cristobalroden@hotmail.com>
2023-01-03 21:02:08 -0300

    transformar diferentes formas de tiempo en millas

---
+++ TiempoMillas.py
@@ -0,0 +1,17 @@
+def dias(dia):
+    return dia * 24 * 60 * 60 *1000
+
+def horas(hora):
+    return hora * 60 * 60 * 1000
+
+def minutos(minuto):
+    return minuto * 60 * 1000
+
+def segundos(segundo):
+    
+    return segundo * 1000
+
+print (dias(1))
+print (horas(1))
+print (minutos(1))
+print (segundos(2))
