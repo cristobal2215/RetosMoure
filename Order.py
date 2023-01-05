e0c8145b4336f6411c3d1bc4299f9561413dd147
cristobal2215 <cristobalroden@hotmail.com>
2023-01-05 11:07:56 -0300

    ordenar lista ascendente y descendente

---
+++ Order.py
@@ -0,0 +1,24 @@
+def Ordenar(numero, asc):
+    meth_sort = []
+
+    for i in numero:
+        added = False
+
+        for j in range(len(meth_sort)):
+            if asc:
+                condition = i < meth_sort[j]
+            else:
+                condition = i > meth_sort[j]
+            
+            if condition:
+                meth_sort.insert(j, i)
+                added = True
+                break
+
+        if not added:
+            meth_sort.append(i)
+    
+    return meth_sort
+
+print(Ordenar([7, 4, 6, 1, 3, 8, 5, 2], True))
+print(Ordenar([7, 4, 6, 1, 3, 8, 5, 2], False))
